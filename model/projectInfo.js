b8a01e99c35de07615f708197bfbdfad8e845d51
Suman Kundu <kundu.1suman@gmail.com>
2025-11-24 00:53:59 +0530

    Dashboard build. Only for active status. Take a projectInfo data model

---
+++ model/projectInfo.js
@@ -0,0 +1,102 @@
+const projects = [
+    {
+        id: 101,
+        name: 'Student Management',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sanjoy Mishra',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 102,
+        name: 'Hospital Management',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sanjoy Mishra',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 103,
+        name: 'Employee Management',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sanjoy Mishra',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 104,
+        name: 'Travel agency',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sanjoy Mishra',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 105,
+        name: 'Restrurent Management',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Vicky ob Roy',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 106,
+        name: 'Job Vacancy',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'David Hassi',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 107,
+        name: 'Online Class Management',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Abhijit Ray',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 108,
+        name: 'Home cleaner',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sujoy Sengupta',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 109,
+        name: 'Online food Delivary',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sujoy Sengupta',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    },
+    {
+        id: 110,
+        name: 'Online Toilet Cleaner Booking',
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+        owner: 'Sujoy Sengupta',
+        baseprice: '$100',
+        startTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        endTime: 'Thu Nov 23 2024 14:30:00 GMT+0530',
+        status: 'Active'
+    }
+]
